3b8bac09faa60feaec24d2a9f571d7941cc14f0e
mjadhav1 <manmohan_jadhav2@trimble.com>
2017-02-10 15:53:23 +0530

    Initial Commit. Seed from angular CLI.

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Welcome to my app..!!';
+}
---
+++ src/app/hero-details/hero-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero.model';
+
+
+@Component({
+	selector: 'app-hero-details',
+	templateUrl: './hero-details.component.html',
+	styleUrls: ['./hero-details.component.css']
+})
+export class HeroDetailsComponent implements OnInit {
+
+	public title: string = 'Hero Details';
+	public hero: Hero = <Hero>{};
+	constructor() { }
+
+	ngOnInit() {
+		
+	}
+}
+
+
